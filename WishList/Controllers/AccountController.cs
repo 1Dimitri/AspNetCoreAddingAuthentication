0cf1076256e421d3d8b856e6b4a026a5274022f2
1Dimitri <git.dimitri@janczak.net>
2021-11-21 18:00:56 +0100

    login logout done

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -57,5 +57,37 @@ namespace WishList.Controllers
 
             return RedirectToAction("Index", "Home");
         }
+
+        [HttpGet,AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost,AllowAnonymous,ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Login", loginViewModel);
+            }
+            Task<Microsoft.AspNetCore.Identity.SignInResult> r = _signInManager.PasswordSignInAsync(loginViewModel.Email,
+                loginViewModel.Password,
+                false,
+                false);
+            if (!r.Result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View("Login",loginViewModel);
+            }
+            return RedirectToAction("Index", "Item");
+        }
+
+        [HttpPost,ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
